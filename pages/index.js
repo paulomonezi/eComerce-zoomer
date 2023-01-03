199a835f83dac6ccf7be0c13fc5470d11949455e
Paulo Monezi <paulo.monezi@icloud.com>
2023-01-03 08:03:58 -0400

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-full min-h-screen bg-slate-900 text-slate-100 flex items-center justify-center">
+      <h1>EmersonBrogaDev</h1>
+    </div>
+  );
+}
